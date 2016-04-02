4ce313e86f8e527b5deaab6cf2468cc4fd59b3ae
Wilton Rodrigues <braynwilton@gmail.com>
2016-04-02 08:26:00 -0300

    Questão 1051 e 1052

---
+++ URI/Iniciante/1051/1051.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	double s, a, b;
+
+	cin >> s;
+
+	if ((s >= 0.00) && (s <= 2000.00)){
+		cout << "Isento" << endl;
+	}
+	else if ((s >= 2000.01) && (s <= 3000.00)){	
+		cout << fixed << (s - 2000.00) * 0.08 << endl;
+	}
+	else if ((s >= 3000.01) && (s <= 4500.00)){
+		a = s - 3000.00;
+		cout.precision(2);
+		cout << fixed << (80.00)+(a*0.18) << endl;
+	}
+	else {
+		b = s - 4500.00;
+		cout.precision(2);
+		cout << fixed << (350.00)+(b*0.28) << endl;
+	}
+
+
+return 0;
+}
