01647b39643f449360571bdd801da7186b7165c1
Wilton Rodrigues <braynwilton@gmail.com>
2016-04-15 10:15:53 -0300

    Adicionando arquivo para rodar diferenças das saídas

--- URI/Iniciante/crdir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read var
-mkdir /home/wilton/Dropbox/UnB/TEP/URI/Iniciante/$var
-touch /home/wilton/Dropbox/UnB/TEP/URI/Iniciante/$var/$var.cpp
-touch /home/wilton/Dropbox/UnB/TEP/URI/Iniciante/$var/in
-cd $var/
